2e2f3d872352492c53823be01ec9fb3606ecbccd
Jacob Runge <jwrunge@gmail.com>
2020-03-16 00:02:35 -0500

    A little more functional

--- src/main.js
+++ src/main.js
@@ -3,50 +3,71 @@ let path = require('path')
 let fs = require('fs')
 
 // List all files in a directory in Node.js recursively in a synchronous fashion
-var walkSync = function(dir, filelist, rootDir = dir) {
+function walkSync(dir, filelist, rootDir = dir) {
     var fs = fs || require('fs'),
-        files = fs.readdirSync(dir);
-    filelist = filelist || [];
+        files = fs.readdirSync(dir)
+    filelist = filelist || []
     files.forEach(function(file) {
         if (fs.statSync(dir + file).isDirectory()) {
-            filelist = walkSync(dir + file + '/', filelist, rootDir);
+            filelist = walkSync(dir + file + '/', filelist, rootDir)
         }
         else {
             var shortdir = dir.replace(rootDir, '')
-            filelist.push(shortdir + file);
+            filelist.push(shortdir + file)
         }
-    });
-    return filelist;
-};
+    })
+    return filelist
+}
 
-let directories = [
-    {dir: '/../images-webp-400/', size: 400},
-    {dir: '/../images-webp-800/', size: 800},
-    {dir: '/../images-webp-1600/', size: 1600},
-    {dir: '/../images-webp-3200/', size: 3200}
-]
+//Destination is the folder containing the image source folder
+//imageFolder is the name of the image folder inside the source folder
+//breakpoints is an array of objects with dirnames and image widths as such:
+/*
+    [
+        { dir: 'images-webp-400', size: 400 },
+        { dir: 'images-webp-800', size: 800 },
+        { dir: 'images-webp-1600', size: 1600 }
+    ]
 
-let images = walkSync(__dirname + '/../images/')
+    creates folders <destination>/images-webp-400 through 1600 and stores images in webp of those sizes
+*/
+export function createOptimizationFolders(destination, imageFolder, breakpoints) {
+    //Error check here! TODO
 
-for(let i = 0; i < images.length; i++) {
-    let fileName = path.parse(images[i]).name
-    let imgExt = path.parse(images[i]).ext
-    let fullName = path.basename(images[i])
-    let shortPath = images[i].replace(fullName, "")
+    if(!breakpoints) {
+        breakpoints = [
+            {dir: path.join(destination, 'images-webp-400'), size: 400},
+            {dir: path.join(destination, 'images-webp-800'), size: 800},
+            {dir: path.join(destination, 'images-webp-1600'), size: 1600},
+            {dir: path.join(destination, 'images-webp-3200'), size: 3200}
+        ]
+    }
 
-    if(imgExt != '.svg') {
-        //Cycle through output folders
-        for(let j = 0; j < directories.length; j++) {
-            //Create the new directory if it does not exist
-            if(!fs.existsSync(__dirname + directories[j].dir + shortPath))
-                fs.mkdirSync(__dirname + directories[j].dir + shortPath, { recursive: true })
+    //Source folder
+    let sourceFolder = path.join(destination, imageFolder)
 
-            sharp(__dirname + '/../images/' + images[i])
-                .resize({ width: directories[j].size, withoutEnlargement: true })
-                .toFile(__dirname + directories[j].dir + shortPath + fileName + ".webp",
-                (err, info)=> {
-                    if(err) console.log(err)
-                })
+    let images = walkSync(sourceFolder)
+
+    for(let i = 0; i < images.length; i++) {
+        let fileName = path.parse(images[i]).name
+        let imgExt = path.parse(images[i]).ext
+        let fullName = path.basename(images[i])
+        let shortPath = images[i].replace(fullName, "")
+    
+        if(imgExt != '.svg') {
+            //Cycle through output folders
+            for(let j = 0; j < directories.length; j++) {
+                //Create the new directory if it does not exist
+                if(!fs.existsSync(path.join(destination, directories[j].dir, shortPath)))
+                    fs.mkdirSync(path.join(destination, directories[j].dir, shortPath), { recursive: true })
+    
+                sharp(path.join(sourceFolder, images[i]))
+                    .resize({ width: directories[j].size, withoutEnlargement: true })
+                    .toFile(path.join(destination, directories[j].dir, shortPath, fileName, ".webp"),
+                    (err, info)=> {
+                        if(err) console.log(err)
+                    })
+            }
         }
     }
 }
